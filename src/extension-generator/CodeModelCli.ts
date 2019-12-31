134d6ecc99890a7dcc4bd59369b56178c53dd4f5
Zim Kalinowski <zikalino@microsoft.com>
2019-12-31 09:46:40 +0800

    internal code model

---
+++ src/extension-generator/CodeModelCli.ts
@@ -0,0 +1,78 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export interface CodeModel
+{
+    SelectFirstExtension(): boolean;
+    SelectNextExtension(): boolean;
+
+    Extension_Name: string;
+    Extension_NameUnderscored: string;
+    Extension_NameClass: string;
+
+    SelectFirstCommandGroup(): boolean;
+    SelectNextCommandGroup(): boolean;
+
+    CommandGroup_Name: string;
+    CommandGroup_NameUnderscored: string;
+    CommandGroup_Help: string;
+    CommandGroup_Commands: string[];
+
+    SelectFirstCommand(): boolean;
+    SelectNextCommand(): boolean;
+    SelectCommand(name: string): boolean;
+
+    Command_Name: string;
+    Command_NameUnderscored: string;
+
+    Command_Help: string;
+
+    SelectFirstOption(): boolean;
+    SelectNextOption(): boolean;
+    HasSubOptions(): boolean;
+    EnterSubOptions(): boolean;
+    ExitSubOptions(): boolean;
+
+    Option_Name: string;
+    Option_NameUnderscored: string;
+    Option_NamePython: string;
+    Option_IsRequired: boolean;
+    Option_Description: string;
+    Option_Type: string;
+    Option_IsList: boolean;
+    Option_PathSdk: string;
+    Option_PathSwagger: string;
+    Option_EnumValues: string[];
+
+    SelectFirstMethod(): boolean;
+    SelectNextMethod(): boolean;
+
+    Method_IsFirst: boolean;
+    Method_IsLast: boolean;
+    Method_Name: string;
+    Method_BodyParameterName: string;
+
+    SelectFirstMethodParameter(): boolean;
+    SelectNextMethodParameter(): boolean;
+
+    MethodParameter_Name: string;
+    MethodParamerer_MapsTo: string;
+
+
+    GetModuleOperationName(): string;
+    GetModuleOperationNameUpper(): string;
+    GetPythonNamespace(): string;
+
+    // Python
+    PythonMgmtClient: string;
+    PythonOperationsName: string;
+
+    SelectFirstExample(): boolean;
+    SelectNextExample(): boolean;
+    FindExampleById(id: string): string[];
+    Example_Body: string[];
+    Example_Title: string;
+    Example_Params: any;
+}
