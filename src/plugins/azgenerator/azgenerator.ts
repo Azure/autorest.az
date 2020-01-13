586e5af8a4d4b50e08482d0dfbf9e18db9038fd5
Zim Kalinowski <zikalino@microsoft.com>
2020-01-13 17:12:49 +0800

    connect generator

--- src/plugins/azgenerator/azgenerator.ts
+++ src/plugins/azgenerator/azgenerator.ts
@@ -1,34 +1,21 @@
 import { CodeModel, codeModelSchema } from '@azure-tools/codemodel';
 import { Session, startSession, Host, Channel } from '@azure-tools/autorest-extension-base';
 import { serialize, deserialize } from '@azure-tools/codegen';
-
-class AzGenerator {
-    codeModel: CodeModel;
-
-    constructor(protected session: Session<CodeModel>) {
-        this.codeModel = session.model;
-    }
-
-
-    async process() {
-
-        //let azSettings = await this.session.getValue('az');
-        //let extensionName = azSettings['az-name'];
-        //console.error(extensionName);
-        this.session.message({Channel:Channel.Debug, Text:"in azgenerator process"});
-
-        return {};
-    }
-}
+import { GenerateAll } from "./Generator";
+import { CodeModelCliImpl } from "./CodeModelAzImpl";
 
 export async function processRequest(host: Host) {
     const debug = await host.GetValue('debug') || false;
     host.Message({Channel:Channel.Warning, Text:"in azgenerator processRequest"});
     try {
         const session = await startSession<CodeModel>(host, {}, codeModelSchema);
-        const plugin = await new AzGenerator(session);
-        const result = await plugin.process();
-        host.WriteFile('azgenerator-temp-output', serialize(result));
+
+        let model = new CodeModelCliImpl();
+        let files: any = GenerateAll(model, [], true, "cli-name");
+
+        for (let f in files) {
+            host.WriteFile(f, files[f].join('\r\n'));
+        }
     } catch (E) {
         if (debug) {
             console.error(`${__filename} - FAILURE  ${JSON.stringify(E)} ${E.stack}`);
