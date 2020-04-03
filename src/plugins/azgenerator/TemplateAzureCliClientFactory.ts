0b1552259cff928cea3e5ea2471f23f948d5480a
Zim Kalinowski <zikalino@microsoft.com>
2020-04-03 11:14:54 +0200

    new az parameters for url bound and subscription bound clients

--- src/plugins/azgenerator/TemplateAzureCliClientFactory.ts
+++ src/plugins/azgenerator/TemplateAzureCliClientFactory.ts
@@ -15,7 +15,17 @@ export function GenerateAzureCliClientFactory(model: CodeModelAz) : string[] {
     output.push("def cf_" + model.Extension_NameUnderscored + "(cli_ctx, *_):");
     output.push("    from azure.cli.core.commands.client_factory import get_mgmt_service_client");
     output.push("    from ..vendored_sdks." + model.PythonOperationsName + " import " + model.PythonMgmtClient);
-    output.push("    return get_mgmt_service_client(cli_ctx, " + model.PythonMgmtClient + ")");
+
+    if (model.Extension_ClientSubscriptionBound || model.Extension_ClientBaseUrlBound)
+    {
+        output.push("    return get_mgmt_service_client(cli_ctx, " + model.PythonMgmtClient + ",");
+        output.push("                                   subscription_bound=" + (model.Extension_ClientSubscriptionBound ? "True" : "False"));
+        output.push("                                   base_url_bound=" + (model.Extension_ClientBaseUrlBound ? "True" : "False") + ")");
+    }
+    else
+    {
+        output.push("    return get_mgmt_service_client(cli_ctx, " + model.PythonMgmtClient + ")");
+    }
 
     if (model.SelectFirstCommandGroup())
     {
@@ -25,6 +35,7 @@ export function GenerateAzureCliClientFactory(model: CodeModelAz) : string[] {
             {
                 output.push("");
                 output.push("");
+
                 output.push("def cf_" + model.GetModuleOperationName() + "(cli_ctx, *_):");
                 output.push("    return cf_" + model.Extension_NameUnderscored + "(cli_ctx)." + model.GetModuleOperationNamePython());
             }
