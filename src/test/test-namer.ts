e63e8d74f52d42cc7b0409a5faa26ba29e2b3991
qiaozha <qiaozha@microsoft.com>
2020-04-10 12:35:14 +0800

    fix az namer test

--- src/test/test-namer.ts
+++ src/test/test-namer.ts
@@ -24,9 +24,7 @@ const resources = `${__dirname}/../../src/test/resources/`;
         const folders = await readdir(resources);
         for(var each of folders) {
             let cfg = {
-                az:{
-                    extensions:each
-                }
+                extensions:each
             }
             const session = await createTestSession<CodeModel>(cfg, resources + "/" + each, [each + '-cli-common.yaml'], []);
     
