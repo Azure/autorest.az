e6db3db0c9d596f26b296b26538ae550136c40b3
Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>
2020-06-19 18:31:19 +0800

    required set for action (#439)

--- src/test/scenarios/datafactory/output/src/datafactory/azext_datafactory/generated/_params.py
+++ src/test/scenarios/datafactory/output/src/datafactory/azext_datafactory/generated/_params.py
@@ -46,12 +46,12 @@ def load_arguments(self, _):
         c.argument('location', arg_type=get_location_type(self.cli_ctx),
                    validator=get_default_location_from_resource_group)
         c.argument('tags', tags_type)
-        c.argument('factory_vsts_configuration', action=AddFactoryVstsConfiguration, nargs='+', help='Factory\'s VSTS '
+        c.argument('factory_vsts_configuration', action=AddFactoryVstsConfiguration, nargs='*', help='Factory\'s VSTS '
                    'repo information.', arg_group='RepoConfiguration')
-        c.argument('factory_git_hub_configuration', action=AddFactoryGitHubConfiguration, nargs='+', help='Factory\'s '
+        c.argument('factory_git_hub_configuration', action=AddFactoryGitHubConfiguration, nargs='*', help='Factory\'s '
                    'GitHub repo information.', arg_group='RepoConfiguration')
-        c.argument('fake_identity', action=AddFakeIdentity, nargs='+', help='This is only for az test.')
-        c.argument('zones', nargs='+', help='This is only for az test.')
+        c.argument('fake_identity', action=AddFakeIdentity, nargs='*', help='This is only for az test.')
+        c.argument('zones', nargs='*', help='This is only for az test.')
 
     with self.argument_context('datafactory update') as c:
         c.argument('resource_group_name', resource_group_name_type)
@@ -65,9 +65,9 @@ def load_arguments(self, _):
     with self.argument_context('datafactory configure-factory-repo') as c:
         c.argument('location_id', help='The location identifier.', id_part='name')
         c.argument('factory_resource_id', help='The factory resource id.')
-        c.argument('factory_vsts_configuration', action=AddFactoryVstsConfiguration, nargs='+', help='Factory\'s VSTS '
+        c.argument('factory_vsts_configuration', action=AddFactoryVstsConfiguration, nargs='*', help='Factory\'s VSTS '
                    'repo information.', arg_group='RepoConfiguration')
-        c.argument('factory_git_hub_configuration', action=AddFactoryGitHubConfiguration, nargs='+', help='Factory\'s '
+        c.argument('factory_git_hub_configuration', action=AddFactoryGitHubConfiguration, nargs='*', help='Factory\'s '
                    'GitHub repo information.', arg_group='RepoConfiguration')
 
     with self.argument_context('datafactory get-data-plane-access') as c:
@@ -197,12 +197,12 @@ def load_arguments(self, _):
         c.argument('if_match', help='ETag of the integration runtime entity. Should only be specified for update, for '
                    'which it should match existing entity or can be * for unconditional update.')
         c.argument('description', help='Integration runtime description.')
-        c.argument('factory_vsts_configuration', action=AddFactoryVstsConfiguration, nargs='+', help='Factory\'s VSTS '
+        c.argument('factory_vsts_configuration', action=AddFactoryVstsConfiguration, nargs='*', help='Factory\'s VSTS '
                    'repo information.', arg_group='RepoConfiguration')
-        c.argument('factory_git_hub_configuration', action=AddFactoryGitHubConfiguration, nargs='+', help='Factory\'s '
+        c.argument('factory_git_hub_configuration', action=AddFactoryGitHubConfiguration, nargs='*', help='Factory\'s '
                    'GitHub repo information.', arg_group='RepoConfiguration')
-        c.argument('fake_identity', action=AddFakeIdentity, nargs='+', help='This is only for az test.')
-        c.argument('zones', nargs='+', help='This is only for az test.')
+        c.argument('fake_identity', action=AddFakeIdentity, nargs='*', help='This is only for az test.')
+        c.argument('zones', nargs='*', help='This is only for az test.')
         c.argument('type_properties_compute_properties', type=validate_file_or_dict, help='The compute resource for '
                    'managed integration runtime. Expected value: json-string/@json-file.')
         c.argument('type_properties_ssis_properties', type=validate_file_or_dict, help='SSIS properties for managed '
--- src/test/scenarios/managed-network/output/src/managed-network/azext_managed_network/generated/_params.py
+++ src/test/scenarios/managed-network/output/src/managed-network/azext_managed_network/generated/_params.py
@@ -119,11 +119,11 @@ def load_arguments(self, _):
                    validator=get_default_location_from_resource_group)
         c.argument('management_groups', type=validate_file_or_dict, help='The collection of management groups covered '
                    'by the Managed Network Expected value: json-string/@json-file.')
-        c.argument('subscriptions', action=AddSubscriptions, nargs='+', help='The collection of subscriptions covered '
+        c.argument('subscriptions', action=AddSubscriptions, nargs='*', help='The collection of subscriptions covered '
                    'by the Managed Network')
-        c.argument('virtual_networks', action=AddVirtualNetworks, nargs='+', help='The collection of virtual nets '
+        c.argument('virtual_networks', action=AddVirtualNetworks, nargs='*', help='The collection of virtual nets '
                    'covered by the Managed Network')
-        c.argument('subnets', action=AddSubnets, nargs='+', help='The collection of  subnets covered by the Managed '
+        c.argument('subnets', action=AddSubnets, nargs='*', help='The collection of  subnets covered by the Managed '
                    'Network')
 
     with self.argument_context('managed-network mn group update') as c:
@@ -134,11 +134,11 @@ def load_arguments(self, _):
                    validator=get_default_location_from_resource_group)
         c.argument('management_groups', type=validate_file_or_dict, help='The collection of management groups covered '
                    'by the Managed Network Expected value: json-string/@json-file.')
-        c.argument('subscriptions', action=AddSubscriptions, nargs='+', help='The collection of subscriptions covered '
+        c.argument('subscriptions', action=AddSubscriptions, nargs='*', help='The collection of subscriptions covered '
                    'by the Managed Network')
-        c.argument('virtual_networks', action=AddVirtualNetworks, nargs='+', help='The collection of virtual nets '
+        c.argument('virtual_networks', action=AddVirtualNetworks, nargs='*', help='The collection of virtual nets '
                    'covered by the Managed Network')
-        c.argument('subnets', action=AddSubnets, nargs='+', help='The collection of  subnets covered by the Managed '
+        c.argument('subnets', action=AddSubnets, nargs='*', help='The collection of  subnets covered by the Managed '
                    'Network')
 
     with self.argument_context('managed-network mn group delete') as c:
@@ -170,9 +170,9 @@ def load_arguments(self, _):
         c.argument('policy_name', help='The name of the Managed Network Peering Policy.')
         c.argument('location', arg_type=get_location_type(self.cli_ctx),
                    validator=get_default_location_from_resource_group)
-        c.argument('hub', action=AddHub, nargs='+', help='Gets or sets the hub virtual network ID')
-        c.argument('spokes', action=AddSpokes, nargs='+', help='Gets or sets the spokes group IDs')
-        c.argument('mesh', action=AddMesh, nargs='+', help='Gets or sets the mesh group IDs')
+        c.argument('hub', action=AddHub, nargs='*', help='Gets or sets the hub virtual network ID')
+        c.argument('spokes', action=AddSpokes, nargs='*', help='Gets or sets the spokes group IDs')
+        c.argument('mesh', action=AddMesh, nargs='*', help='Gets or sets the mesh group IDs')
 
     with self.argument_context('managed-network managed-network-peering-policy mesh-topology create') as c:
         c.argument('resource_group_name', resource_group_name_type)
@@ -180,9 +180,9 @@ def load_arguments(self, _):
         c.argument('policy_name', help='The name of the Managed Network Peering Policy.')
         c.argument('location', arg_type=get_location_type(self.cli_ctx),
                    validator=get_default_location_from_resource_group)
-        c.argument('hub', action=AddHub, nargs='+', help='Gets or sets the hub virtual network ID')
-        c.argument('spokes', action=AddSpokes, nargs='+', help='Gets or sets the spokes group IDs')
-        c.argument('mesh', action=AddMesh, nargs='+', help='Gets or sets the mesh group IDs')
+        c.argument('hub', action=AddHub, nargs='*', help='Gets or sets the hub virtual network ID')
+        c.argument('spokes', action=AddSpokes, nargs='*', help='Gets or sets the spokes group IDs')
+        c.argument('mesh', action=AddMesh, nargs='*', help='Gets or sets the mesh group IDs')
 
     with self.argument_context('managed-network managed-network-peering-policy hub-and-spoke-topology update') as c:
         c.argument('resource_group_name', resource_group_name_type)
@@ -190,9 +190,9 @@ def load_arguments(self, _):
         c.argument('policy_name', help='The name of the Managed Network Peering Policy.', id_part='child_name_1')
         c.argument('location', arg_type=get_location_type(self.cli_ctx),
                    validator=get_default_location_from_resource_group)
-        c.argument('hub', action=AddHub, nargs='+', help='Gets or sets the hub virtual network ID')
-        c.argument('spokes', action=AddSpokes, nargs='+', help='Gets or sets the spokes group IDs')
-        c.argument('mesh', action=AddMesh, nargs='+', help='Gets or sets the mesh group IDs')
+        c.argument('hub', action=AddHub, nargs='*', help='Gets or sets the hub virtual network ID')
+        c.argument('spokes', action=AddSpokes, nargs='*', help='Gets or sets the spokes group IDs')
+        c.argument('mesh', action=AddMesh, nargs='*', help='Gets or sets the mesh group IDs')
         c.ignore('managed_network_peering_policy_name', 'properties')
 
     with self.argument_context('managed-network managed-network-peering-policy mesh-topology update') as c:
@@ -201,9 +201,9 @@ def load_arguments(self, _):
         c.argument('policy_name', help='The name of the Managed Network Peering Policy.', id_part='child_name_1')
         c.argument('location', arg_type=get_location_type(self.cli_ctx),
                    validator=get_default_location_from_resource_group)
-        c.argument('hub', action=AddHub, nargs='+', help='Gets or sets the hub virtual network ID')
-        c.argument('spokes', action=AddSpokes, nargs='+', help='Gets or sets the spokes group IDs')
-        c.argument('mesh', action=AddMesh, nargs='+', help='Gets or sets the mesh group IDs')
+        c.argument('hub', action=AddHub, nargs='*', help='Gets or sets the hub virtual network ID')
+        c.argument('spokes', action=AddSpokes, nargs='*', help='Gets or sets the spokes group IDs')
+        c.argument('mesh', action=AddMesh, nargs='*', help='Gets or sets the mesh group IDs')
         c.ignore('managed_network_peering_policy_name', 'properties')
 
     with self.argument_context('managed-network managed-network-peering-policy delete') as c:
