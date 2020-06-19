4585132460f8cf8247d9f18eb8d414459bbe5afb
Liang Wang <lianw@microsoft.com>
2020-06-19 17:43:37 +0800

    Add Sample for Release Test (#447)

--- .scripts/e2e/conftest.py
+++ .scripts/e2e/conftest.py
@@ -1,10 +1,13 @@
+from distutils.dir_util import copy_tree
 import pathlib
 
 def pytest_addoption(parser):
-    parser.addoption("--rp", action="store", default="defaultRP")
-    parser.addoption("--az", action="store", default="defaultAZ")
-    parser.addoption("--swagger", action="store", default="defaultSwagger")
-    parser.addoption("--cliext", action="store", default="defaultCliextf")
+    parser.addoption("--rp", action="store", default="")
+    parser.addoption("--az", action="store", default="")
+    parser.addoption("--swagger", action="store", default="")
+    parser.addoption("--cliext", action="store", default="")
+    parser.addoption("--azpack", action="store", default="")
+    parser.addoption("--testserver", action="store", default="")
     parser.addoption("--all", action="store_true", help="run all combinations")
 
 def pytest_generate_tests(metafunc):
@@ -12,6 +15,8 @@ def pytest_generate_tests(metafunc):
     az_path = metafunc.config.option.az;
     swagger_path = metafunc.config.option.swagger;
     cliext_path = metafunc.config.option.cliext;
+    azpack_path = metafunc.config.option.azpack;
+    testserver_path = metafunc.config.option.testserver;
     if 'rp' in metafunc.fixturenames:
         if metafunc.config.getoption("all"):
             path = pathlib.Path(__file__).parent.absolute().joinpath("rp_list");
@@ -27,6 +32,12 @@ def pytest_generate_tests(metafunc):
     if 'az' in metafunc.fixturenames:
         if az_path is not None:
             print("Az: " + az_path);
+            if testserver_path is not None:
+                print("TestServer: " + testserver_path)
+                from_path = testserver_path + '/swagger/'
+                to_path = az_path + '/src/test/scenarios/testserver/input/'
+                print("Copy from " + from_path + " to " + to_path);
+                copy_tree(from_path, to_path)
             metafunc.parametrize("az", [az_path])
     if 'swagger' in metafunc.fixturenames:
         if swagger_path is not None:
@@ -36,3 +47,7 @@ def pytest_generate_tests(metafunc):
         if cliext_path is not None:
             print("Cliext: " + cliext_path);
             metafunc.parametrize("cliext", [cliext_path])
+    if 'azpack' in metafunc.fixturenames:
+        if azpack_path is not None:
+            print("Azpack: " + azpack_path);
+            metafunc.parametrize("azpack", [azpack_path])
