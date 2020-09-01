bdfeda611f9e67054562117c3263db58e52398fe
Changlong Liu <changlong.liu@microsoft.com>
2020-09-01 11:12:38 +0800

    WIP

--- src/utils/inplace.ts
+++ src/utils/inplace.ts
@@ -3,12 +3,12 @@ import {readFileSync, existsSync} from 'fs';
 export class TargetFile {
     content: string;
     currentAt: number;
-    children: BaseSegment[];
+    root: BaseSegment;
 
     public constructor(fileContent: string[]) {
         this.content = fileContent.join("\n");
         this.currentAt = 0;
-        this.children = [];
+        this.root = new BaseSegment(this, 0, fileContent.length);
     }
 
     public createSegments() {
@@ -35,23 +35,55 @@ export class TargetFile {
     }
 
     public merge(originA: TargetFile, customizedA: TargetFile) {
-
+        for (let i=0; i<originA.children.length;i++) {
+            let originSeg = originA.children[i];
+            originSeg.status = SegmentStatus.Deleted;
+            for (let j=0; j<customizedA.children.length; j++) {
+                let customizedSeg = customizedA.children[j];
+                if (originSeg.name == customizedSeg.name) {
+                    if (originSeg.Content == customizedSeg.Content) {
+                        originSeg.status = SegmentStatus.Origin;
+                    }
+                    else {
+                        originSeg.status = SegmentStatus.Origin;
+                    }
+                    break
+                }
+            }
+        }
     }
 }
 
+export enum SegmentStatus {
+    Origin,
+    Modified,
+    Deleted,
+}
 
 export class BaseSegment {
     children: BaseSegment[];
     startAt: number;
     endAt: number;
-    id: string;
+    name: string;
+    status: SegmentStatus;
+    target: TargetFile;
+    customized: BaseSegment;
 
-    public constructor(startAt: number, endAt: number, name: string="") {
+    public constructor(target: TargetFile,startAt: number, endAt: number, name: string="") {
         this.startAt = startAt;
         this.endAt = endAt;
         this.children = [];
-        this.id = this.constructor.name+ "_"+name;
+        this.name = this.constructor.name+ "_"+name;
+        this.status = SegmentStatus.Origin;
+        this.target = target;
+        this.customized = undefined;
     }
+
+    public get Content() {
+        return this.target.content.slice(this.startAt, this.endAt);
+    }
+
+    public status
 }
 
 export class HeadSegment extends BaseSegment {
@@ -62,7 +94,7 @@ export class HeadSegment extends BaseSegment {
                 nextAt = target.content.length-1;
             }
             nextAt += 1;
-            target.children.push(new HeadSegment(target.currentAt, nextAt));
+            target.children.push(new HeadSegment(target, target.currentAt, nextAt));
             target.currentAt = nextAt;
             return true;
         }
@@ -83,11 +115,38 @@ export class testStepSegment extends DefSegment {
                 nextAt = target.content.length-1;
             }
             nextAt +=  target.currentAt + 1;
-            target.children.push(new testStepSegment(target.currentAt, nextAt, remain.slice(3, remain.indexOf("\n"))));
+            target.children.push(new testStepSegment(target, target.currentAt, nextAt, remain.slice(3, remain.indexOf("\n"))));
             target.currentAt = nextAt;
             return true;
         }
         return false;
+    }
+
+    public createChildren(target: TargetFile) {
+        let seperators = [
+            ["endwith", ":\n", "declare"],
+            ["startwith", "    test.cmd(", "pre"],
+            ["endwith", "checks=[", "command"],
+            ["startwith", "])\n", "checkers"],
+        ];
+
+        let bias = this.startAt;
+        let t, tag, name;
+        for (let seperator of seperators) {
+            t = seperator[0];
+            tag = seperator[1];
+            name = seperator[2];
+            let idx = target.content.indexOf(tag, bias);
+            if (t=="endwith") {
+                this.children.push(new BaseSegment(target, bias, idx+tag.length, name));
+                bias = idx + tag.length;
+            }
+            else if (t=="startwith") {
+                this.children.push(new BaseSegment(target, bias, idx, name));
+                bias = idx;
+            }
+        }
+        this.children.push(new BaseSegment(target, bias, this.endAt, "post"));
     }
 }
 
@@ -98,7 +157,7 @@ export class TailSegment extends BaseSegment {
             return false;
         }
         const nextAt = target.content.length;
-        target.children.push(new TailSegment(target.currentAt, nextAt));
+        target.children.push(new TailSegment(target, target.currentAt, nextAt));
         target.currentAt = nextAt;
         return true;
     }
