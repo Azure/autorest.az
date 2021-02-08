a6b4b93ad6c4b3c88dd981e6eda684daa4b479a7
qiaozha <qiaozha@microsoft.com>
2021-02-08 20:43:27 +0800

    fix-test

--- src/python/setup.ts
+++ src/python/setup.ts
@@ -1,24 +1,11 @@
 import * as cp from 'child_process';
-import { isNullOrUndefined } from '../utils/helper';
 import * as extension from '@azure-tools/extension';
 
 export async function runPython3(scriptName, debug = '') {
     const command = ['python', scriptName, debug];
     await extension.updatePythonPath(command);
     const commandScript = command.join(' ');
-    return await new Promise<boolean>((resolve, reject) => {
-        cp.exec(commandScript, function (error) {
-            if (!isNullOrUndefined(error)) {
-                console.log(error.message);
-                console.log(error.stack);
-                console.log('exec error: ' + error);
-                // Reject if there is an error:
-                return reject(false);
-            }
-            // Otherwise resolve the promise:
-            return resolve(true);
-        });
-    });
+    cp.execSync(commandScript);
 }
 
 if (require.main === module) {
