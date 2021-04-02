6021d350d077f80bb1ebc3d4089dde205143b8af
changlong-liu <59815250+changlong-liu@users.noreply.github.com>
2021-04-02 10:07:15 +0800

    max&min test (#790)
    * max&min test
    
    * test
    
    Co-authored-by: Qiaoqiao Zhang <55688292+qiaozha@users.noreply.github.com>

--- test/scenarios/datafactory/output/src/datafactory-preview/azext_datafactory_preview/tests/cmdlet/test_positive.py
+++ test/scenarios/datafactory/output/src/datafactory-preview/azext_datafactory_preview/tests/cmdlet/test_positive.py
@@ -18,51 +18,51 @@ class PositiveTest(ScenarioTest):
     def __init__(self, *args, **kwargs):
         super(PositiveTest, self).__init__(*args, **kwargs)
 
-    # EXAMPLE: /Factories/get/Factories_ListByResourceGroup
     def test_list2(self):
+        # From /Factories/get/Factories_ListByResourceGroup
         self.cmd('az datafactory list '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /Factories/get/Factories_List
     def test_list(self):
+        # From /Factories/get/Factories_List
         self.cmd('az datafactory list')
 
-    # EXAMPLE: /Factories/get/Factories_Get
     def test_show(self):
+        # From /Factories/get/Factories_Get
         self.cmd('az datafactory show '
                  '--name "myFactory" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /Factories/put/Factories_CreateOrUpdate
     def test_create(self):
+        # From /Factories/put/Factories_CreateOrUpdate
         self.cmd('az datafactory create '
                  '--location "East US" '
                  '--zones "earth" "moon" '
                  '--name "myFactory" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /Factories/patch/Factories_Update
     def test_update(self):
+        # From /Factories/patch/Factories_Update
         self.cmd('az datafactory update '
                  '--name "myFactory" '
                  '--tags exampleTag="exampleValue" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /Factories/delete/Factories_Delete
     def test_delete(self):
+        # From /Factories/delete/Factories_Delete
         self.cmd('az datafactory delete -y '
                  '--name "myFactory" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /Factories/post/Factories_ConfigureFactoryRepo
     def test_configure_factory_repo(self):
+        # From /Factories/post/Factories_ConfigureFactoryRepo
         self.cmd('az datafactory configure-factory-repo '
                  '--factory-resource-id "/subscriptions/12345678-1234-1234-1234-12345678abc/resourceGroups/myResourceGroup/providers/Microsoft.DataFactory/factories/myFactory" '
                  '--factory-vsts-configuration "FactoryVSTSConfiguration" "project" "" "ADF" "repo" "/" "master" '
                  '--location-id "East US"')
 
-    # EXAMPLE: /Factories/post/Factories_GetDataPlaneAccess
     def test_get_data_plane_access(self):
+        # From /Factories/post/Factories_GetDataPlaneAccess
         self.cmd('az datafactory get-data-plane-access '
                  '--name "myFactory" '
                  '--access-resource-path "" '
@@ -72,8 +72,8 @@ class PositiveTest(ScenarioTest):
                  '--start-time "2018-11-10T02:46:20.2659347Z" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /Factories/post/Factories_GetGitHubAccessToken
     def test_get_git_hub_access_token(self):
+        # From /Factories/post/Factories_GetGitHubAccessToken
         self.cmd('az datafactory get-git-hub-access-token '
                  '--name "myFactory" '
                  '--git-hub-access-code "some" '
@@ -81,107 +81,107 @@ class PositiveTest(ScenarioTest):
                  '--git-hub-client-id "some" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /Triggers/get/Triggers_ListByFactory
     def test_trigger_list(self):
+        # From /Triggers/get/Triggers_ListByFactory
         self.cmd('az datafactory trigger list '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /Triggers/get/Triggers_Get
     def test_trigger_show(self):
+        # From /Triggers/get/Triggers_Get
         self.cmd('az datafactory trigger show '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup" '
                  '--name "myTrigger"')
 
-    # EXAMPLE: /Triggers/put/Triggers_Create
     def test_trigger_create(self):
+        # From /Triggers/put/Triggers_Create
         self.cmd('az datafactory trigger create '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup" '
                  '--properties "{{\\"type\\":\\"ScheduleTrigger\\",\\"pipelines\\":[{{\\"parameters\\":{{\\"OutputBlobNameList\\":[\\"exampleoutput.csv\\"]}},\\"pipelineReference\\":{{\\"type\\":\\"PipelineReference\\",\\"referenceName\\":\\"examplePipeline\\"}}}}],\\"typeProperties\\":{{\\"recurrence\\":{{\\"endTime\\":\\"2018-06-16T00:55:13.8441801Z\\",\\"frequency\\":\\"Minute\\",\\"interval\\":4,\\"startTime\\":\\"2018-06-16T00:39:13.8441801Z\\",\\"timeZone\\":\\"UTC\\"}}}}}}" '
                  '--name "myTrigger"')
 
-    # EXAMPLE: /Triggers/put/Triggers_Update
     def test_trigger_update(self):
+        # From /Triggers/put/Triggers_Update
         self.cmd('az datafactory trigger update '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup" '
                  '--description "Example description" '
                  '--name "myTrigger"')
 
-    # EXAMPLE: /Triggers/delete/Triggers_Delete
     def test_trigger_delete(self):
+        # From /Triggers/delete/Triggers_Delete
         self.cmd('az datafactory trigger delete -y '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup" '
                  '--name "myTrigger"')
 
-    # EXAMPLE: /Triggers/post/Triggers_GetEventSubscriptionStatus
     def test_trigger_get_event_subscription_status(self):
+        # From /Triggers/post/Triggers_GetEventSubscriptionStatus
         self.cmd('az datafactory trigger get-event-subscription-status '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup" '
                  '--name "myTrigger"')
 
-    # EXAMPLE: /Triggers/post/Triggers_QueryByFactory
     def test_trigger_query_by_factory(self):
+        # From /Triggers/post/Triggers_QueryByFactory
         self.cmd('az datafactory trigger query-by-factory '
                  '--factory-name "myFactory" '
                  '--parent-trigger-name "myTrigger" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /Triggers/post/Triggers_Start
     def test_trigger_start(self):
+        # From /Triggers/post/Triggers_Start
         self.cmd('az datafactory trigger start '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup" '
                  '--name "myTrigger"')
 
-    # EXAMPLE: /Triggers/post/Triggers_Stop
     def test_trigger_stop(self):
+        # From /Triggers/post/Triggers_Stop
         self.cmd('az datafactory trigger stop '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup" '
                  '--name "myTrigger"')
 
-    # EXAMPLE: /Triggers/post/Triggers_SubscribeToEvents
     def test_trigger_subscribe_to_event(self):
+        # From /Triggers/post/Triggers_SubscribeToEvents
         self.cmd('az datafactory trigger subscribe-to-event '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup" '
                  '--name "myTrigger"')
 
-    # EXAMPLE: /Triggers/post/Triggers_UnsubscribeFromEvents
     def test_trigger_unsubscribe_from_event(self):
+        # From /Triggers/post/Triggers_UnsubscribeFromEvents
         self.cmd('az datafactory trigger unsubscribe-from-event '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup" '
                  '--name "myTrigger"')
 
-    # EXAMPLE: /IntegrationRuntimes/get/IntegrationRuntimes_ListByFactory
     def test_integration_runtime_list(self):
+        # From /IntegrationRuntimes/get/IntegrationRuntimes_ListByFactory
         self.cmd('az datafactory integration-runtime list '
                  '--factory-name "myFactory" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/get/IntegrationRuntimes_Get
     def test_integration_runtime_show(self):
+        # From /IntegrationRuntimes/get/IntegrationRuntimes_Get
         self.cmd('az datafactory integration-runtime show '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/put/IntegrationRuntimes_Create
     def test_integration_runtime_self_hosted_create(self):
+        # From /IntegrationRuntimes/put/IntegrationRuntimes_Create
         self.cmd('az datafactory integration-runtime self-hosted create '
                  '--factory-name "myFactory" '
                  '--description "A selfhosted integration runtime" '
                  '--name "myIntegrationRuntime" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/patch/IntegrationRuntimes_Update
     def test_integration_runtime_update(self):
+        # From /IntegrationRuntimes/patch/IntegrationRuntimes_Update
         self.cmd('az datafactory integration-runtime update '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
@@ -189,87 +189,87 @@ class PositiveTest(ScenarioTest):
                  '--auto-update "Off" '
                  '--update-delay-offset "\\"PT3H\\""')
 
-    # EXAMPLE: /IntegrationRuntimes/delete/IntegrationRuntimes_Delete
     def test_integration_runtime_delete(self):
+        # From /IntegrationRuntimes/delete/IntegrationRuntimes_Delete
         self.cmd('az datafactory integration-runtime delete -y '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_GetConnectionInfo
     def test_integration_runtime_get_connection_info(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_GetConnectionInfo
         self.cmd('az datafactory integration-runtime get-connection-info '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_GetMonitoringData
     def test_integration_runtime_get_monitoring_data(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_GetMonitoringData
         self.cmd('az datafactory integration-runtime get-monitoring-data '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_GetStatus
     def test_integration_runtime_get_status(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_GetStatus
         self.cmd('az datafactory integration-runtime get-status '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_ListAuthKeys
     def test_integration_runtime_list_auth_key(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_ListAuthKeys
         self.cmd('az datafactory integration-runtime list-auth-key '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_RegenerateAuthKey
     def test_integration_runtime_regenerate_auth_key(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_RegenerateAuthKey
         self.cmd('az datafactory integration-runtime regenerate-auth-key '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--key-name "authKey2" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_RemoveLinks
     def test_integration_runtime_remove_link(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_RemoveLinks
         self.cmd('az datafactory integration-runtime remove-link '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--linked-factory-name "exampleFactoryName-linked" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_Start
     def test_integration_runtime_start(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_Start
         self.cmd('az datafactory integration-runtime start '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime2" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_Stop
     def test_integration_runtime_stop(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_Stop
         self.cmd('az datafactory integration-runtime stop '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime2" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_SyncCredentials
     def test_integration_runtime_sync_credentials(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_SyncCredentials
         self.cmd('az datafactory integration-runtime sync-credentials '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /IntegrationRuntimes/post/IntegrationRuntimes_Upgrade
     def test_integration_runtime_upgrade(self):
+        # From /IntegrationRuntimes/post/IntegrationRuntimes_Upgrade
         self.cmd('az datafactory integration-runtime upgrade '
                  '--factory-name "myFactory" '
                  '--name "myIntegrationRuntime" '
                  '--resource-group "myResourceGroup"')
 
-    # EXAMPLE: /datafactory linked-integration-runtime/post/IntegrationRuntimes_CreateLinkedIntegrationRuntime
     def test_linked_integration_runtime_create(self):
+        # From /datafactory linked-integration-runtime/post/IntegrationRuntimes_CreateLinkedIntegrationRuntime
         self.cmd('az datafactory linked-integration-runtime create '
                  '--name "myDatafactoryLinkedIntegrationRuntime" '
                  '--data-factory-location "West US" '
